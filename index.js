f1055555b80abd82976f4021fc8b618409b934b1
detbin <116663162+detbin@users.noreply.github.com>
2022-12-29 14:48:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const boton=document.querySelector("#btn")
+p=5
+console.log(p)
+boton.addEventListener("click",()=>{
+    alert('click en el botón');
+    console.log("hola estoy usando Jquery")
+})
